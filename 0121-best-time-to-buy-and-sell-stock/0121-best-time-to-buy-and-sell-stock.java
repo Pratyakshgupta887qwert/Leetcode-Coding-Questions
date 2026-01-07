db97018b582cf992de110d9f8ea9e3b210defe4d
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-07 15:13:17 +0530

    Time: 1 ms (99.75%), Space: 94.6 MB (14.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -37,15 +37,78 @@
 // }
 
 
+// class Solution{
+//     public int maxProfit(int[] prices){
+//         int minPrice = Integer.MAX_VALUE;
+//         int maxProfit = 0;
+//         for(int i=0;i<prices.length;i++){
+//             if(prices[i]<minPrice)
+//                 minPrice = prices[i];
+//             maxProfit = Math.max(prices[i]-minPrice,maxProfit);
+//         }
+//         return maxProfit;
+//     }
+// }
+
+//cctp 
+//brute force code.
+// class Solution{
+//     public int maxProfit(int[] prices){
+//         //buy 7
+//         // sell 7
+//         // max=0
+//         // buy = 1
+//         // sell = 5 
+//         // max profit =6
+
+//         // buy = 5
+
+//         //sell = 6
+//         //max profit = 1
+
+//         //buy = 3
+//         // sell = 6
+//         // max = 3
+
+//         // so on....
+
+//         int max=0;
+//         for(int i=0;i<prices.length;i++){
+//             int buy=prices[i];
+//             int sell = prices[i];
+//             for(int j=i+1;j<prices.length;j++){   
+//                 if(prices[j]>sell){
+//                     sell=prices[j];
+//                     // sell =Math.max(sell,prices[j]);
+//                 }
+//                 //or can be written like this # removing the  if else
+//                 int profit=sell-buy;
+//                 max=Math.max(max,profit);
+//             }
+//         }
+//         return max;   
+//     }
+// }
+
+
 class Solution{
     public int maxProfit(int[] prices){
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+        int max=0;
+        int buy=Integer.MAX_VALUE;
+
+        // past sei smallest sei linge and sell max profit par karinge.
+
+        //buy=7;
+
+        //buy=1;
+        // max profit : 5
+
         for(int i=0;i<prices.length;i++){
-            if(prices[i]<minPrice)
-                minPrice = prices[i];
-            maxProfit = Math.max(prices[i]-minPrice,maxProfit);
+            buy=Math.min(buy,prices[i]);
+            int sell = prices[i];
+            int profit=sell-buy;
+            max=Math.max(max,profit);
         }
-        return maxProfit;
+        return max;
     }
 }
