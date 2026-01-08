14a15ee93afa9380c4f68391248497da079c6a1e
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-08 14:22:42 +0530

    Time: 36 ms (41.41%), Space: 46.8 MB (64.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,9 +6,28 @@ class Solution {
                 if(nums[i]+nums[j]==target){
                     ans[0]=i;
                     ans[1]=j;
+                    //break : it is not imp to write for optimization we write this 
+                    break;
                 }
             }
         }
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int ans[]=new int[2];
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]==target){
+//                     return new int[]{i,j}*************************** most impt direct array.
+//                     //break : it is not imp to write for optimization we write this 
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
