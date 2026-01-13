ad4daadc3e43a28bf0a0b03b97f5fd4560db62be
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-13 14:15:32 +0530

    Time: 457 ms (10.72%), Space: 44.1 MB (13.43%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // RAG X (regular expression) is used for pattern matching.
+        return s.matches(p);  // this methord is used to check that it maches the pattern
+
+
+        
+
+    }
+}
