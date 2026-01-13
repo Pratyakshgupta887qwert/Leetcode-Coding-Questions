7f35e1b6bc1c0f98742a554bc883aec4ed7ffbac
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-13 13:57:07 +0530

    Time: 0 ms (100%), Space: 43.1 MB (38.52%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,26 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         s=s.trim();
+//         String words[]=s.split(" ");
+//         return words[words.length-1].length();
+//     }
+// }
+
+
 class Solution {
+
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        String words[]=s.split(" ");
-        return words[words.length-1].length();
+        s = s.trim();
+        int l = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+            l++;
+        }
+
+        return l;
     }
+
 }
