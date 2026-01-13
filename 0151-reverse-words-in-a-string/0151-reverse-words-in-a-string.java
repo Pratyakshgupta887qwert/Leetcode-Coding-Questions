0b6b9efd49e8f3877fced608aa0a7f6d16cc3afc
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-13 13:40:58 +0530

    Time: 88 ms (5.43%), Space: 45.5 MB (22.33%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,22 +1,38 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//       // sentence mein sei words  alag karna (for many functions like how many time it comes in the sentence and like this question )
+//         String words[]=s.split(" ");
+//         String ns="";
+//         for(int i=words.length-1;i>=0;i--){
+//             if(words[i].length()==0)continue;
+//             ns+=words[i]+" ";
+//         }
+//         return ns.trim();
+//     }
+    
+// }
+
+
+// /*
+// string is immutable (can not be updated but we can do this via copying this and making changes in this)
+
+// string name = "ram";
+// name = name.substring(0,1)+"b"+name.substring(2);
+
+// */
+
+
+
 class Solution {
     public String reverseWords(String s) {
-      // sentence mein sei words  alag karna (for many functions like how many time it comes in the sentence and like this question )
         String words[]=s.split(" ");
-        String ns="";
+        StringBuilder ns=new StringBuilder("");
         for(int i=words.length-1;i>=0;i--){
             if(words[i].length()==0)continue;
-            ns+=words[i]+" ";
+            ns.append(words[i]+" ");
         }
-        return ns.trim();
+        return ns.toString().trim();
     }
     
 }
 
-
-/*
-string is immutable (can not be updated but we can do this via copying this and making changes in this)
-
-string name = "ram";
-name = name.substring(0,1)+"b"+name.substring(2);
-
-*/
