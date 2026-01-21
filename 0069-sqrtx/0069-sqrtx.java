989a709c9c93ee66f2c3b54423137e5029bf8e53
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-01-21 11:09:42 +0530

    Time: 1 ms (96.51%), Space: 43 MB (14.77%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
         // return (int) Math.sqrt(x);
-
+// we will get the error : lossy conversion , so we will use type conversion in this (int).
         // or
         return (int) Math.pow(x,0.5);
     }
