6e91e96674fdd7aef8b7dffffff7fc36096c9e73
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-03 14:23:41 +0530

    Time: 11 ms (5.8%), Space: 49.8 MB (6.73%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,26 +1,33 @@
-// 2 pointer
+// // 2 pointer
 
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+//         StringBuilder sb = new StringBuilder();
 
-        int i = 0, j = 0;
-        while (i < word1.length() && j < word2.length()) {
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(j));
-            i++;
-            j++;
-        }
-        if (i < word1.length()) {
-            sb.append(word1.substring(i));
-        }
-        if (j < word2.length()) {
-            sb.append(word2.substring(j));
-        }
+//         int i = 0, j = 0;
+//         while (i < word1.length() && j < word2.length()) {
+//             sb.append(word1.charAt(i));
+//             sb.append(word2.charAt(j));
+//             i++;
+//             j++;
+//         }
+//         if (i < word1.length()) {
+//             sb.append(word1.substring(i));
+//         }
+//         if (j < word2.length()) {
+//             sb.append(word2.substring(j));
+//         }
 
-        return sb.toString();
-    }
-}
+//         return sb.toString();
+//     }
+// }
+
+
+
+
+
+
+
 
 // single pointer
 
@@ -48,3 +55,26 @@ class Solution {
 //         return result.toString();
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+class Solution{
+    public String mergeAlternately(String word1, String word2){
+        int i=0,j=0;
+        String ans="";
+        while(i<word1.length()&&j<word2.length()){
+            ans=ans+word1.charAt(i)+word2.charAt(j);
+            i++;j++;
+        }
+        while(i<word1.length())ans+=word1.charAt(i++);
+        while(j<word2.length())ans+=word2.charAt(j++);
+        return ans;
+    }
+}
