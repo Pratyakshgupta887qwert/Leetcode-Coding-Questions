be214722967556baaf6440b36da2df961f30e924
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-10 14:19:55 +0530

    Time: 14 ms (14.86%), Space: 46.6 MB (95.94%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -19,6 +19,29 @@
 
 
 // doing this question by hashmap.
+// import java.util.HashMap;
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for (int i : nums) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
+//         }
+
+//         for (int key : map.keySet()) {
+//             if (map.get(key) == 1) {
+//                 return key;
+//             }
+//         }
+
+//         return -1; // fallback (won't hit as per problem constraints)
+//     }
+// }
+
+
+
+
 import java.util.HashMap;
 
 class Solution {
@@ -26,15 +49,14 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i : nums) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+            int c=map.getOrDefault(i, 0);
+            map.put(i,c+1);
         }
-
-        for (int key : map.keySet()) {
-            if (map.get(key) == 1) {
-                return key;
-            }
+        for(int k:map.keySet()){
+            int fr=map.get(k);
+            if(fr==1) return k;
         }
-
+        
         return -1; // fallback (won't hit as per problem constraints)
     }
 }
