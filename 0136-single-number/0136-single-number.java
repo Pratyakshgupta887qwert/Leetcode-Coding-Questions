3f826461a360c6786731f9669baabb5891e266af
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-10 14:19:46 +0530

    Time: 14 ms (14.86%), Space: 46.9 MB (63.23%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,40 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int num : nums) {
-            ans ^= num;
-        }
-        return ans;
-    }
-}
+// with simple xor methord'
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int ans = 0;
+//         for (int num : nums) {
+//             ans ^= num;
+//         }
+//         return ans;
+//     }
+// }
 
 
 // other way of doing this question is : by using hashmap 
 // frequency = 1 will be return
 
+
+
+
+
+// doing this question by hashmap.
+import java.util.HashMap;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+
+        return -1; // fallback (won't hit as per problem constraints)
+    }
+}
