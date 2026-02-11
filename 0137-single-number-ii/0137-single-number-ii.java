b0ed32c16d509fd89a6b56e31c624df34e87ae39
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-11 11:13:39 +0530

    Time: 6 ms (43.97%), Space: 47 MB (9.69%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int result=0;
+//         for(int i=0;i<32;i++){
+//             int sum=0;
+//             for(int num:nums){
+//                 if(((num>>i)&1)==1){
+//                     sum++;
+
+//                 }
+//             }
+//             if(sum%3!=0){
+//                 result|=(1<<i);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
+
+// by hashmap
 class Solution {
     public int singleNumber(int[] nums) {
-        int result=0;
-        for(int i=0;i<32;i++){
-            int sum=0;
-            for(int num:nums){
-                if(((num>>i)&1)==1){
-                    sum++;
-
-                }
-            }
-            if(sum%3!=0){
-                result|=(1<<i);
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        return result;
+        for(int k:map.keySet()){
+            if(map.get(k)==1)return k;
+        }
+        return -1;
     }
 }
