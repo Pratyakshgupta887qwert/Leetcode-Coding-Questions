fbcbe8ea0d785587c12abc63ff710c7eb709f752
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-11 10:56:13 +0530

    Time: 5 ms (21.43%), Space: 47.4 MB (64.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -34,14 +34,32 @@
 
 
 
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         //with the help of sorting
+//         int n=nums.length;
+//         boolean arr[]=new boolean[n+1];
+//         for(int i:nums)arr[i]=true;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==false)return i;
+
+//         }
+//         return -1;
+//     }
+// }
+
+
+//by hashset.
+
 class Solution {
     public int missingNumber(int[] nums) {
         //with the help of sorting
         int n=nums.length;
-        boolean arr[]=new boolean[n+1];
-        for(int i:nums)arr[i]=true;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==false)return i;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums) set.add(i);
+        
+        for(int i=0;i<=n;i++){
+            if(!set.contains(i))return i;
 
         }
         return -1;
