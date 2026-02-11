856c028ce94d18bbad78b0546c42962d2a70e515
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-11 10:50:05 +0530

    Time: 0 ms (100%), Space: 47.4 MB (45.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -11,16 +11,39 @@
 
 
 
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         //with the help of sorting
+//         int n=nums.length;
+//         int expected=n*(n+1)/2;
+//         int actual=0;
+//         for(int num :nums){
+//             actual+=num;
+
+//         }
+//         return expected-actual;
+//     }
+// }
+
+
+
+// this question can also be done by XOR.
+
+
+
+
+
+
 class Solution {
     public int missingNumber(int[] nums) {
         //with the help of sorting
         int n=nums.length;
-        int expected=n*(n+1)/2;
-        int actual=0;
-        for(int num :nums){
-            actual+=num;
+        boolean arr[]=new boolean[n+1];
+        for(int i:nums)arr[i]=true;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==false)return i;
 
         }
-        return expected-actual;
+        return -1;
     }
 }
