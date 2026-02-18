ae0a82a5d79f0555c8e4c1e82bf84006a8722291
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-02-18 10:21:50 +0530

    Time: 15 ms (11.1%), Space: 47.3 MB (5.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,39 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int max=0;
+//         for(int i=0;i<s.length();i++){
+//             Set<Character> st=new HashSet<>();
+//             for(int j=i;j<s.length();j++){
+//                 char ch=s.charAt(j);
+
+//                 if(st.contains(ch))break;
+//                 st.add(ch);
+
+//             }
+//             max=Math.max(max,st.size());
+//         }
+//         return max;
+//     }
+// }
+
+
+
+
+// frequency array.(variable side sliding window).
+class Solution{
+    public int lengthOfLongestSubstring(String s){
         int max=0;
         for(int i=0;i<s.length();i++){
-            Set<Character> st=new HashSet<>();
+            int fr[]=new int[256];
+            int count=0;
             for(int j=i;j<s.length();j++){
                 char ch=s.charAt(j);
-
-                if(st.contains(ch))break;
-                st.add(ch);
-
+                fr[ch]++;
+                if(fr[ch]>1)break;
+                count++;
             }
-            max=Math.max(max,st.size());
+            max=Math.max(max,count);
         }
         return max;
     }
 }
-
