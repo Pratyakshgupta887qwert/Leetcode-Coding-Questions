bbe22f20ca385976fc709acadb43a65eda00e496
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-16 09:45:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,14 +1,36 @@
+// class Solution {
+//     public int numSubarrayProductLessThanK(int[] nums, int k) {
+//         int count =0;
+//         for (int i=0;i<nums.length;i++){
+//             int p= 1;
+//             for(int j=i;j<nums.length;j++){
+//                 p*=nums[j];
+//                 if(p>=k)break;
+//                 count++;
+
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
+// sliding window
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int count =0;
-        for (int i=0;i<nums.length;i++){
-            int p= 1;
-            for(int j=i;j<nums.length;j++){
-                p*=nums[j];
-                if(p>=k)break;
-                count++;
-
+        if (k <= 1) return 0;
+        int count = 0;
+        int product = 1;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+            while (product >= k && left <= right) {
+                product /= nums[left];
+                left++;
             }
+            count += (right - left + 1);
         }
         return count;
     }
