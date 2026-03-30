15c590f09a2c44597a343ce6459c42e8cfe64021
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-30 09:41:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,17 +1,40 @@
+// class Solution {
+//     public int[] nextGreaterElements(int[] nums) {
+//         int n = nums.length;
+//         int ans[]=new int[nums.length];
+//         Arrays.fill(ans,-1);
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=0;j<nums.length;j++){
+//                 int idx=(i+j)%n;
+//                 if(nums[idx]>nums[i]){
+//                     ans[i]=nums[idx];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int ans[]=new int[nums.length];
         Arrays.fill(ans,-1);
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                int idx=(i+j)%n;
-                if(nums[idx]>nums[i]){
-                    ans[i]=nums[idx];
-                    break;
-                }
+        Stack<Integer> st= new Stack<>();
+        for(int i=0;i<nums.length*2;i++){
+            int idx=i%n;
+            while(!st.isEmpty()&& nums[idx]>nums[st.peek()]){
+                int j=st.pop();
+                ans[j]=nums[idx];
             }
+            st.push(idx);
         }
         return ans;
     }
+
 }
