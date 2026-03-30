9561876356d56c3d8af087387ed859cd3f24b1db
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-30 09:56:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -9,7 +9,7 @@ class Solution{
 // class Solution {
 //     public boolean rotateString(String s, String goal) {
 //         for(int i=1;i<s.length();i++){
-//             String rot=s.substring(i)+s.substring(0,1);
+//             String rot=s.substring(i)+s.substring(0,i);
 //             if(rot.equals(goal)) return true;
 //         }
 //         return false;
