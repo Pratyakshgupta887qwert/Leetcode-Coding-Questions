9479992e14c7641f44bb76bc474faa8ac9aa922a
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-03-30 08:49:31 +0530

    Time: 78 ms (14.99%), Space: 106.7 MB (71.17%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,44 @@
+/* in this we have find the next greater temp. : hint */
+
+// in this TLE is coming
+
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         int ans[]=new int[temperatures.length];
+//         for(int i=0;i<temperatures.length;i++){
+//             for(int j=i+1;j<temperatures.length;j++){
+//                 if(temperatures[i]<temperatures[j]){
+//                     ans[i]=j-i;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+// optimized
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int ans[]=new int[temperatures.length];
+        Stack<Integer> st= new Stack<>();
+        for(int i=temperatures.length-1;i>=0;i--){
+            while(!st.isEmpty()&&temperatures[st.peek()]<=temperatures[i]){
+             
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=0;
+            }
+            else{
+                ans[i]=st.peek()-i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
