4f52578fd28240f34d042107dc7fdadf0b8e5fad
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-06 09:11:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,21 +1,41 @@
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         //how to find the length of ans array; length of example - k (+1).
+//         int[] ans=new int[nums.length-k+1];
+        
+//         for(int i=0;i<=nums.length-k;i++){
+//             int max=Integer.MIN_VALUE;
+//             for(int j=i;j<i+k;j++){
+//                 max=Math.max(max,nums[j]);
+//             }
+//             ans[i]=max;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+//doubly ended queue.
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> q = new ArrayDeque<>();  // stores *indices*
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            while (!q.isEmpty() && nums[q.getLast()] <= nums[i]) {
-                q.removeLast();
+        int n=nums.length;
+        int [] ans=new int[n-k+1];
+
+        Deque<Integer> q=new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!q.isEmpty() && q.peekFirst() <= i-k){
+                q.pollFirst();
+
+            }
+            while(!q.isEmpty() && nums[q.peekLast()]<nums[i]){
+                q.pollLast();
             }
             q.addLast(i);
-            // remove first element if it's outside the window
-            if (q.getFirst() == i - k) {
-                q.removeFirst();
-            }
-            // if window has k elements add to results (first k-1 windows have < k elements because we start from empty window and add 1 element each iteration)
-            if (i >= k - 1) {
-                res.add(nums[q.peek()]);
+            if(i>=k-1){
+                ans[i-k+1]=nums[q.peekFirst()];
             }
         }
-        return res.stream().mapToInt(i->i).toArray();            
+        return ans;
     }
 }
