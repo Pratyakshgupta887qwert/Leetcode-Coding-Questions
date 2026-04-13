cc7084a25e831b57a98cfe334b3f929d7a8784c4
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-13 09:24:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.java
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -1,3 +1,31 @@
+// // gcd : greatest common divisor.
+// class Solution {
+//     public boolean canMeasureWater(int x, int y, int target) {
+//         if(target > x + y) return false;
+//         int gcd = gcd(x, y);
+//         return target % gcd == 0;
+//     }
+
+//     // Euclidean algorithm for GCD
+//     int gcd(int x, int y){
+//         int dividend = x;
+//         int divisor = y;
+
+//         while(dividend % divisor != 0){
+//             int rem = dividend % divisor;
+//             dividend = divisor;
+//             divisor = rem;
+//         }
+//         return divisor;
+//     }
+// }
+
+
+
+
+
+
+
 // gcd : greatest common divisor.
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
@@ -5,17 +33,10 @@ class Solution {
         int gcd = gcd(x, y);
         return target % gcd == 0;
     }
-
-    // Euclidean algorithm for GCD
-    int gcd(int x, int y){
-        int dividend = x;
-        int divisor = y;
-
-        while(dividend % divisor != 0){
-            int rem = dividend % divisor;
-            dividend = divisor;
-            divisor = rem;
-        }
-        return divisor;
+// by recursion
+    int gcd(int dividend, int divisor){
+        if(divisor == 0) return dividend;
+        return gcd(divisor, dividend % divisor);
     }
+    
 }
