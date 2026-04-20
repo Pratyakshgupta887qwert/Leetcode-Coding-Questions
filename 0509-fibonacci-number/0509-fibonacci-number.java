ef638e55f2bc4f6d7db81fbe05138e52737d3d0a
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-20 13:05:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -7,14 +7,3 @@ class Solution {
         return fib(N-1) + fib(N-2);
     }
 }
-
-
-// class Solution {
-//     public int fib(int N) {
-//         if(N<=1)
-//         return N;
-//         if(N==2)
-//             return 1;
-//         return fib(N-1) + fib(N-2);
-//     }
-// }
