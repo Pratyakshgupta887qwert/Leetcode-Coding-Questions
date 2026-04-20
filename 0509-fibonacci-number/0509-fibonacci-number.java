69d660388dc09d4ddea623b77adbf60da703b111
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-20 13:27:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -8,15 +8,25 @@
 //     }
 // }
 
-class Solution{
-    public int fib(int N){
-        int a=0,b=1;
-        for(int i=0;i<N;i++){
-            int c = a+b;
-            a=b;
-            b=c;
-        }
-        return a;
+// class Solution{
+//     public int fib(int N){
+//         int a=0,b=1;
+//         for(int i=0;i<N;i++){
+//             int c = a+b;
+//             a=b;
+//             b=c;
+//         }
+//         return a;
 
+//     }
+// }
+
+
+
+class Solution {
+    public int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return fib(N - 1) + fib(N - 2);
     }
 }
