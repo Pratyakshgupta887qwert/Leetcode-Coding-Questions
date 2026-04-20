a7920f73ea64012b6c3779ddd7da77ef2da5142c
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-20 13:15:47 +0530

    Time: 53 ms (9.45%), Space: 42.3 MB (6.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,22 @@
-class Solution {
-    public int fib(int N) {
-        if(N<=1)
-        return N;
-        if(N==2)
-            return 1;
-        return fib(N-1) + fib(N-2);
+// class Solution {
+//     public int fib(int N) {
+//         if(N<=1)
+//         return N;
+//         if(N==2)
+//             return 1;
+//         return fib(N-1) + fib(N-2);
+//     }
+// }
+
+class Solution{
+    public int fib(int N){
+        int a=0,b=1;
+        for(int i=0;i<N;i++){
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+        return a;
+
     }
 }
