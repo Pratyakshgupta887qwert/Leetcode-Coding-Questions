f281f348324ccafdbacd572031b5672687851d7e
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-22 11:09:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -33,21 +33,45 @@
 
 
 
+// class Solution {
+//     public int tribonacci(int n) {
+//         int dp[] = new int[n + 1];
+//         return tri(n, dp);
+//     }
+
+//     public int tri(int n, int dp[]){
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         if(n==2) return 1;
+
+//         if(dp[n] != 0) return dp[n];
+
+//         dp[n] = tri(n-1, dp) + tri(n-2, dp) + tri(n-3, dp);
+
+//         return dp[n];
+//     }
+// }
+
+
+
+// time complex. of both the dp is same'
+//buttom up dp is faster than drop down dp.
+
+
+//buttom up DP
 class Solution {
     public int tribonacci(int n) {
-        int dp[] = new int[n + 1];
-        return tri(n, dp);
-    }
-
-    public int tri(int n, int dp[]){
         if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 1;
+        if(n==1 || n==2) return 1;
 
-        if(dp[n] != 0) return dp[n];
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
 
-        dp[n] = tri(n-1, dp) + tri(n-2, dp) + tri(n-3, dp);
-
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
         return dp[n];
     }
 }
