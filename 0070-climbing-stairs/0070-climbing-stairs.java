187094edcf408c5a6bd735f5256e383cdc962f28
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-22 11:32:51 +0530

    Time: 0 ms (100%), Space: 41.9 MB (74.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,28 +1,32 @@
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 3) return n;
+// class Solution {
+//     public int climbStairs(int n) {
+//         return fun(0,n);
+//     }
 
-        int prev1 = 3;
-        int prev2 = 2;
-        int cur = 0;
-
-        for (int i = 3; i < n; i++) {
-            cur = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = cur;
-        }
-
-        return cur;        
-    }
-}
-// class solution{
-//     public int climbStairs(int n){
-//         int [] dp=new int[n+1];
-//         dp[0]=1;
-//         dp[1]=1;
-//         for(int i=2;i<=n;i++){
-//             dp[i]=dp[i-1]+dp[i-2];
-//         }
-//         return dp[n];
+//     int fun(int i,int n){
+//         if(i==n) return 1;
+//         if(i>n) return 0;
+//         int ways1=fun(i+1,n);
+//         int ways2=fun(i+2,n);
+//         return ways1+ways2;
 //     }
 // }
+
+
+
+// DP 
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        return fun(0, n, dp);
+    }
+
+    int fun(int i, int n, int dp[]){
+        if(i == n) return 1;
+        if(i > n) return 0;
+        if(dp[i] != 0) return dp[i];
+        int ways1 = fun(i+1, n, dp);
+        int ways2 = fun(i+2, n, dp);
+        return dp[i] = ways1 + ways2;
+    }
+}
