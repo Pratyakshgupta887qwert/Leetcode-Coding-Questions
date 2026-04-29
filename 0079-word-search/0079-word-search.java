e6cb752ccbf2d7b388c57cec3d5f367be857662d
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-04-29 11:14:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,28 +3,28 @@ class Solution {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(fun(board, word, i, j)) return true;
+                    if(fun(board, word, i, j,0)) return true;
                 }
             }
         }
         return false;
     }
 
-    boolean fun(char[][] board, String word, int r, int c){
-        if(word.length() == 0) return true;
+    boolean fun(char[][] board, String word, int r, int c,int idx){
+        if(word.length() == idx) return true;
 
         if(r < 0 || r >= board.length || c < 0 || c >= board[0].length) return false;
 
-        if(board[r][c] != word.charAt(0)) return false;
+        if(board[r][c] != word.charAt(idx)) return false;
 
         char ch = board[r][c];
         board[r][c] = ' ';
 
         boolean flag = false;
-        flag |= fun(board, word.substring(1), r - 1, c);
-        flag |= fun(board, word.substring(1), r + 1, c);
-        flag |= fun(board, word.substring(1), r, c - 1);
-        flag |= fun(board, word.substring(1), r, c + 1);
+        flag |= fun(board, word, r - 1, c,idx+1);
+        flag |= fun(board, word, r + 1, c,idx+1);
+        flag |= fun(board, word, r, c - 1,idx+1);
+        flag |= fun(board, word, r, c + 1,idx+1);
 
         board[r][c] = ch;
         return flag;
