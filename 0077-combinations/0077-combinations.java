301f747f87b2c327311f8836d32f6956f3337197
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-06-09 13:57:14 +0530

    Time: 24 ms (13.12%), Space: 98.2 MB (27.48%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,3 +1,30 @@
+// recursion + backtracing
+
+// class Solution {
+//     public List<List<Integer>> combine(int n, int k) {
+//         List<Integer> nums=new ArrayList<>();
+//         for(int i=1;i<=n;i++) nums.add(i);
+//         List<List<Integer>> ans=new ArrayList<>();
+//         fun(nums,ans,k,new ArrayList<> (),0);
+//         return ans;
+
+//     }
+//     void fun(List<Integer>nums,List<List<Integer>> ans,int k,List<Integer>c,int idx){
+//         if(c.size()==k){
+//             ans.add(new ArrayList<>(c));
+//             return;
+//         }
+//         for(int i=idx;i<nums.size();i++){
+//             int num=nums.get(i);
+//             c.add(num);
+//             fun(nums,ans,k,c,i+1);
+//             c.remove(c.size()-1);
+//         }
+//     }
+// }
+
+
+
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<Integer> nums=new ArrayList<>();
@@ -12,11 +39,14 @@ class Solution {
             ans.add(new ArrayList<>(c));
             return;
         }
-        for(int i=idx;i<nums.size();i++){
-            int num=nums.get(i);
-            c.add(num);
-            fun(nums,ans,k,c,i+1);
-            c.remove(c.size()-1);
-        }
+        if(idx>=nums.size())return;
+        //exclude
+        fun(nums,ans,k,c,idx+1);
+
+        //include
+        c.add(nums.get(idx));
+        fun(nums,ans,k,c,idx+1);
+        c.remove(c.size()-1);
+
     }
 }
