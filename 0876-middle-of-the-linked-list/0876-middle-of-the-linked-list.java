474647bcdfa9ed1c979d384b94e406b9fedab07f
Pratyaksh Gupta <150325858+Pratyakshgupta887qwert@users.noreply.github.com>
2026-06-17 09:59:26 +0530

    Time: 0 ms (100%), Space: 42.8 MB (71.48%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,13 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+
+
+
+ // Tortoise and Hare algorithm
+
+
+
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode slow=head;
+//         ListNode fast=head;
+//             while(fast !=null && fast.next!=null){
+//                 slow=slow.next;
+//                 fast=fast.next.next;
+//             }
+//             return slow;
+//     }
+// }
+
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
+       ArrayList<ListNode> list=new ArrayList<>();
+       ListNode temp=head;
+       while(temp!=null){
+        list.add(temp);
+        temp=temp.next;
+       }
+       int n=list.size()/2;
+       return list.get(n);
+       
 
-        while (f != null && f.next != null) {
-            s = s.next;
-            f = f.next.next;
-        }
-
-        return s; 
     }
 }
